ec5bc27f95af689c4b220b183bc4b3dcd5e496b8
earl celis <copongcopong@gmail.com>
2021-08-18 01:32:53 +0800

    added test link

--- src/pages/app/Base.vue
+++ src/pages/app/Base.vue
@@ -5,6 +5,7 @@
       <router-link to="/">home</router-link> |
       <router-link to="/about">about</router-link> |
       <router-link to="/app">app</router-link> |
+      <router-link to="/app?qid=3">app?qid=3</router-link> |
       <router-link to="/app/pars/1">p 1</router-link> |
       <router-link to="/app/pars/2">p 2</router-link> |
       <router-link to="/app/pars/3?qid=1">q 2</router-link> |
