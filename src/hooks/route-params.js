8b6912d253f9c97ad418f4ef4f833a5d04b8b568
earl celis <copongcopong@gmail.com>
2021-08-18 01:54:10 +0800

    use as symbol

--- src/hooks/route-params.js
+++ src/hooks/route-params.js
@@ -4,25 +4,27 @@ import { ref, watch, onUnmounted } from 'vue';
 export { useRouteParams, useQueryParams };
 
 function useRouteParams (key, cb) {
-  
+  const u = Symbol(key);
   const route = useRoute();
-  const param = ref(route.params[key]);
+  const param = {};
+  param[u] = ref(route.params[key]);
   const stop = watch(() => route.params[key], async (nval, oval) => {
-      param.value = nval;
+      param[u].value = nval;
       if (cb) cb(nval, oval)
     });
   onUnmounted(() => stop());
-  return param;
+  return param[u];
 }
 
 function useQueryParams (key, cb) {
-  
+  const u = Symbol(key);
   const route = useRoute();
-  const param = ref(route.query[key]);
+  const param = {};
+  param[u] = ref(route.query[key]);
   const stop = watch(() => route.query[key], async (nval, oval) => {
-      param.value = nval;
+      param[u].value = nval;
       if (cb) cb(nval, oval)
     });
   onUnmounted(() => stop());
-  return param;
+  return param[u];
 }
