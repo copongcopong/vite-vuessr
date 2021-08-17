f13f8dc39022d11e3403b9f09e6041b22956992f
earl celis <copongcopong@gmail.com>
2021-08-18 01:20:55 +0800

    added onUnmount to stop watcher

--- src/hooks/route-params.js
+++ src/hooks/route-params.js
@@ -1,27 +1,28 @@
 import { useRoute } from 'vue-router';
-import { ref, watch } from 'vue';
+import { ref, watch, onUnmounted } from 'vue';
 
 export { useRouteParams, useQueryParams };
+
 function useRouteParams (key, cb) {
   
   const route = useRoute();
   const param = ref(route.params[key]);
-    watch(() => route.params[key], async (nval, oval) => {
+  const stop = watch(() => route.params[key], async (nval, oval) => {
       param.value = nval;
       if (cb) cb(nval, oval)
     });
-
-    return param;
+  onUnmounted(() => stop());
+  return param;
 }
 
 function useQueryParams (key, cb) {
   
   const route = useRoute();
   const param = ref(route.query[key]);
-    watch(() => route.query[key], async (nval, oval) => {
+  const stop = watch(() => route.query[key], async (nval, oval) => {
       param.value = nval;
       if (cb) cb(nval, oval)
     });
-
-    return param;
+  onUnmounted(() => stop());
+  return param;
 }
--- src/pages/app/Pars.vue
+++ src/pages/app/Pars.vue
@@ -11,7 +11,7 @@ import { useAuth } from '@/stores/auth';
 import { usePageState } from '@/hooks/page-state';
 import { useRouteParams, useQueryParams } from '@/hooks/route-params';
 import { useRoute } from 'vue-router';
-import { ref, watch } from 'vue';
+import { ref, watch, onUnmounted } from 'vue';
 
 export default {
 
@@ -22,11 +22,19 @@ export default {
     //to test, click to /pars/1 then click to /pars/2; parsId in template should change
     //use case like trigger new server call to change data show
     const route = useRoute();
+    console.log(route.path, route.name, route.fullPath, route)
     const parsId = ref(route.params.id);
-    watch(() => route.params.id, async (nval, oval) => {
+    const stop = watch(() => route.params.id, async (nval, oval) => {
       console.log('watch', {oval, nval});
       parsId.value = nval;
     });
+    //needed to stop the watcher
+    onUnmounted(() => {
+      console.log('unmounted')
+      stop()
+    });
+    //--- end pattern doc
+
 
     //new composable/hook for a reactive route params
     const pId = useRouteParams('id', (n, v) => {
@@ -35,7 +43,7 @@ export default {
 
     //new composable/hook for a reactive query params
     const qId = useQueryParams('qid', (n, v) => {
-      console.log('pId', {n, v})
+      console.log('qId', {n, v})
     })
 
     console.log('setup', parsId, route.params, route.query);
