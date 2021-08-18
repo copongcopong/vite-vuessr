ede29cf714d370268a7bcae63c58743e0b5284dc
earl celis <copongcopong@gmail.com>
2021-08-18 14:05:51 +0800

    refactor

--- src/hooks/route-params.js
+++ src/hooks/route-params.js
@@ -1,27 +1,27 @@
 import { useRoute } from 'vue-router';
 import { ref, watch, onUnmounted } from 'vue';
 
+const types = ['params', 'query'];
+
 export { useRouteParams, useQueryParams };
 
 function useRouteParams (key, cb) {
-  const u = Symbol(key);
-  const route = useRoute();
-  const param = {};
-  param[u] = ref(route.params[key]);
-  const stop = watch(() => route.params[key], async (nval, oval) => {
-      param[u].value = nval;
-      if (cb) cb(nval, oval)
-    });
-  onUnmounted(() => stop());
-  return param[u];
+  return _use(key, cb, 'params');
 }
 
 function useQueryParams (key, cb) {
-  const u = Symbol(key);
+  return _use(key, cb, 'query');
+}
+
+function _use (key, cb, type) {
+  if (types.indexOf(type) < 0) {
+    throw new Error('Type must be `query` or `params`');
+  }
+  const u = Symbol(`${key}${type}`);
   const route = useRoute();
   const param = {};
-  param[u] = ref(route.query[key]);
-  const stop = watch(() => route.query[key], async (nval, oval) => {
+  param[u] = ref(route[type][key]);
+  const stop = watch(() => route[type][key], async (nval, oval) => {
       param[u].value = nval;
       if (cb) cb(nval, oval)
     });
