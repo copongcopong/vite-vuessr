6eab51d83327c2c221ee40eae2ca5a23cea7a119
earl celis <copongcopong@gmail.com>
2022-01-29 13:57:14 +0800

    up

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { createRouter } from './router';
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 
+import '/node_modules/primeflex/primeflex.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css'; 
